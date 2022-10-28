5e5bb0f44ff6057252679e0785a02bfd5abfbbf1
José A. Espinosa <joseantem@gmail.com>
2022-10-28 10:27:21 +0200

    Update getPATSTAT to fit new API

--- getPATSTAT.py
+++ getPATSTAT.py
@@ -1,104 +1,131 @@
 """
 Created on Feb 24 2021
-Author: Jeronimo Arenas Garcia
-        jarenas@ing.uc3m.es
+Updated on Oct 2022
+Authors:    Jeronimo Arenas Garcia <jarenas@ing.uc3m.es>
+            José Antonio Espinosa Melchor <joespino@pa.uc3m.es>
 
 Download PATSTAT products using REST API
 """
 
-import sys
 import argparse
 import configparser
-import xml.etree.ElementTree as ET
-import requests
-import xmltodict
-import zipfile
 import io
+import json
+import sys
+import zipfile
 from pathlib import Path
 
-if __name__ == "__main__":
+import requests
+from dateutil import parser
 
-    parser = argparse.ArgumentParser(description='Download PATSTAT products using REST API')
-    parser.add_argument('-c','--config', help='Configuration file to use', default='config.cf')
-    parser.add_argument('-p','--path', help='Path where the datasets will be downloaded')
-    argus = parser.parse_args()
+
+def print_status(act_msg, err_msg, resp):
+    print(f"{act_msg}: {resp}")
+    if not resp.status_code == 200:
+        print(err_msg)
+        try:
+            print(json.dumps(json.loads(resp.content.decode()), indent=2))
+        except:
+            pass
+        sys.exit()
+
+
+if __name__ == "__main__":
+    # Args
+    arg_parser = argparse.ArgumentParser(
+        description="Download PATSTAT products using REST API"
+    )
+    arg_parser.add_argument(
+        "-c", "--config", help="Configuration file to use", default="config.cf"
+    )
+    arg_parser.add_argument(
+        "-p", "--path", help="Path where the datasets will be downloaded"
+    )
+    args = arg_parser.parse_args()
 
     # Make sure a valid a configuration file is available
     config = configparser.ConfigParser()
-    if not Path(argus.config).is_file():
-        print('Please provide a valid configuration file')
+    if not Path(args.config).is_file():
+        print("Please provide a valid configuration file")
         sys.exit()
-    config.read( argus.config )
+    config.read(args.config)
 
     # Load credentials and destination path
-    user = config['creds']['user']
-    password = config['creds']['pass']
-
-    if argus.path:
-        rawpath = Path(argus.path)
+    username = config["creds"]["user"]
+    password = config["creds"]["pass"]
+    if args.path:
+        download_path = Path(args.path)
     else:
-        rawpath = Path(config['data']['path'])
-
-    if not rawpath.is_dir():
-        print('Please provide a link to a folder for the download')
+        download_path = Path(config["data"]["path"])
+    if not download_path.is_dir():
+        print("Please provide a link to a folder for the download")
         sys.exit()
 
-    # Authenticate into the system
-    s = requests.Session()
-    url = 'https://publication.epo.org/raw-data/authentication?login=' + \
-           user + '&pwd=' + password + '&action=1&format=1'
-    print('Connect:', url)
-    r = s.get(url)
-    dict_data = xmltodict.parse(r.content)
-    
-    if dict_data['download-area']['authentication']['@authenticated'] == 'true':
-        #authenticated
-        #Retrieve products available for download
-        print('Download products:', url)
-        url = 'https://publication.epo.org/raw-data/products'
-        r = s.get(url)
-        dict_data = xmltodict.parse(r.content)
-        nextUrl = None
-        for product in dict_data['download-area']['products']['product']:
-            if 'PATSTAT Global' in product['name']:
-                nextUrl = product['url']
-        
-        #If PATSTAT Global is available for download, retrieve links
-        if nextUrl:
-            r = s.get(nextUrl+'/editions')
-            dict_data = xmltodict.parse(r.content)
-            version = dict_data['download-area']['editions']['edition']['version'].replace(' ','_')
-            url = dict_data['download-area']['editions']['edition']['url']
-            print('Available edition:', version)
-            version_path = rawpath.joinpath(version)
-            
-            #If the directory name is taken, it is likely that the version has already
-            #been downloaded
-            if version_path.is_dir():
-                print('The destination folder already exists. Delete and try again')
-            else:
-                version_path.mkdir()
-                r = s.get(url)
-                dict_data = xmltodict.parse(r.content)
-                url = dict_data['download-area']['edition']['files-url']
-                r = s.get(url)
-                dict_data = xmltodict.parse(r.content)
-                #SHAS = [el['checksum'] for el in dict_data['download-area']['files']['file']]
-                files = [el['url'] for el in dict_data['download-area']['files']['file']]
-                
-                #Download and extract all files to destination folder
-                for file in files:
-                    print('Donwloading file:', file.split('/')[-1])
-                    r = s.get(file)
-                    z = zipfile.ZipFile(io.BytesIO(r.content))
-                    z.extractall(version_path)
-        else:
-            print('These credentials do not allow access to PATSTAT Global')
+    # Create session
+    with requests.Session() as s:
+        # Get token
+        # Credentials
+        credentials = {
+            "username": username,
+            "password": password,
+            "grant_type": "password",
+            "scope": "openid",
+        }
+        p = s.post(
+            "https://login.epo.org/oauth2/aus3up3nz0N133c0V417/v1/token",
+            headers={
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Authorization": "Basic MG9hM3VwZG43YW41cE1JOE80MTc=",
+            },
+            data=credentials,
+        )
+        print_status("Get token", "Invalid credentials", p)
+        token_response = json.loads(p.content.decode())
+        token_type = token_response["token_type"]
+        access_token = token_response["access_token"]
 
-        # Lastly we need to disconnect
-        url = 'https://publication.epo.org/raw-data/authentication?login=' + \
-              user + '&pwd=' + password + '&action=0&format=1'
-        print('Disconnect:', url)
-        r = s.get(url)
-    else:
-        print('Credentials are not valid')
+        # Get products
+        base_uri = "https://publication-bdds.apps.epo.org/bdds/bdds-bff-service/prod/api/products/"
+        r = s.get(base_uri, headers={"Authorization": f"{token_type} {access_token}"})
+        print_status("Get products", "Invalid subscription products", r)
+        products = json.loads(r.content)
+
+        # Get most recent PATSTAT Global
+        product = [p for p in products if "PATSTAT Global" in p["name"]]
+        if not product:
+            print("These credentials do not allow access to PATSTAT Global")
+            sys.exit()
+        product = product[0]
+        productID = product["id"]
+        r = s.get(f"{base_uri}{productID}")
+        print_status("Get product", "Invalid subscription product", r)
+        data_dict = json.loads(r.content)
+
+        # Filter most recent
+        most_recent = sorted(
+            data_dict["deliveries"],
+            key=lambda x: parser.parse(x["deliveryPublicationDatetime"]),
+            reverse=True,
+        )[0]
+        deliveryId = most_recent["deliveryId"]
+
+        # Create download directory
+        edition = "_".join(most_recent["deliveryName"].split()[-2:])
+        version_path = download_path.joinpath(f"{edition}")
+        if version_path.exists():
+            print("This version has already been downloaded.")
+            print("Closing...")
+            sys.exit()
+        else:
+            # Download
+            version_path.mkdir(parents=True)
+            for file in most_recent["files"]:
+                # Save and extract files
+                fileId = file["fileId"]
+                print(f'Downloading: {file["fileName"]}')
+                print(file)
+                r = s.get(
+                    f"{base_uri}{productID}/delivery/{deliveryId}/file/{fileId}/download"
+                )
+                z = zipfile.ZipFile(io.BytesIO(r.content))
+                z.extractall(version_path)
